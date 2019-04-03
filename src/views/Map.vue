84140a74ebf9ebc4a6b14ab283de79ee80a40e41
dolgovdv <dolgov.denis@gmail.com>
2019-04-04 00:56:39 +0300

    add map
    Signed-off-by: dolgovdv <dolgov.denis@gmail.com>

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
     <h1>Map</h1>
+    <div id="mapid" ref="mapElement"></div>
   </div>
 </template>
+
+<script>
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+export default {
+  name: 'MapLeaflet',
+  daata() {
+    return {
+      mymap: L.map(this.$refs.mapElement).setView([51.505, -0.09], 13)
+    }
+  }
+}
+</script>
+
+<style>
+#mapid {
+  height: 600px;
+  width: 800px;
+}
+</style>
