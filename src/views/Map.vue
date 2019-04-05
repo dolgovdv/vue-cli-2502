75a61aaea091727aef436c0c6ab7184cd8e19ffe
dolgovdv <dolgov.denis@gmail.com>
2019-04-06 02:50:57 +0300

    в карту добавлены маркеры
    Signed-off-by: dolgovdv <dolgov.denis@gmail.com>

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Map</h1>
-    <div id="mapid" ref="mapElement"></div>
+    <div id="map"></div>
   </div>
 </template>
 
@@ -9,19 +9,53 @@
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+})
+
 export default {
   name: 'MapLeaflet',
-  daata() {
+  data() {
     return {
-      mymap: L.map(this.$refs.mapElement).setView([51.505, -0.09], 13)
+      map: null,
+      tileLayer: null,
+      marker: [{ coords: [55.75, 37.61] }, { coords: [55.75, 37.51] }]
+    }
+  },
+  mounted() {
+    /* Code to run when app is mounted */
+    this.initMap()
+  },
+  methods: {
+    /* Any app-specific functions go here */
+    initMap() {
+      this.map = L.map('map', {
+        center: [55.75, 37.61],
+        zoom: 11,
+        layers: this.layer
+      })
+      this.tileLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy;'
+      })
+      this.tileLayer.addTo(this.map)
+      this.addMarker(this.marker)
+    },
+    addMarker(arr) {
+      arr.forEach(element => {
+        L.marker(element.coords).addTo(this.map)
+      })
     }
   }
 }
 </script>
 
 <style>
-#mapid {
+#map {
   height: 600px;
-  width: 800px;
 }
 </style>
