804f961d61033564b60d322b23b00071ec412af0
dolgovdv <dolgov.denis@gmail.com>
2019-04-07 21:06:42 +0300

    Добавление изменения цвета иконки. Без реактивност
    Signed-off-by: dolgovdv <dolgov.denis@gmail.com>

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -24,8 +24,22 @@ export default {
     return {
       map: null,
       tileLayer: null,
-      markers: L.markerClusterGroup(),
-      marker: [{ coords: [55.75, 37.61] }, { coords: [55.75, 37.51] }]
+      markers: null,
+      marker: [
+        {
+          coords: [55.75, 37.61],
+          alarm: null
+        },
+        {
+          coords: [55.75, 37.62],
+          alarm: null
+        },
+        {
+          coords: [55.755, 37.62],
+          alarm: null
+        }
+      ],
+      ClasterClass: 'mycluster'
     }
   },
   mounted() {
@@ -50,12 +64,39 @@ export default {
       this.addMarker(this.marker)
     },
     addMarker(arr) {
+      this.markers = L.markerClusterGroup({
+        // отображение полилинии границы кластера
+        showCoverageOnHover: true,
+        // измененеие иконки маркера кластера
+        iconCreateFunction: function(cluster) {
+          let ClasterClass = 'mycluster'
+          console.log(cluster.getAllChildMarkers())
+          // получение объектов кластера
+          let markers = cluster.getAllChildMarkers()
+
+          for (var i = 0; i < markers.length; i++) {
+            console.log(markers[i].alarm)
+            if (markers[i].alarm !== null) {
+              ClasterClass = 'myclusterAlarm'
+            }
+          }
+          return L.divIcon({
+            html: '<b>' + cluster.getChildCount() + '</b>',
+            className: ClasterClass,
+            iconSize: L.point(40, 40)
+          })
+        },
+        singleMarkerMode: false
+      })
       arr.forEach(element => {
+        // формирование маркеров на слое кластера
+        let markerLayerClaster = L.marker(element.coords)
+        markerLayerClaster.alarm = element.alarm
         // L.marker(element.coords).addTo(this.map)
         // marker claster
-        this.markers.addLayer(L.marker(element.coords))
+        this.markers.addLayer(markerLayerClaster)
       })
-
+      // добавление маркеров на слой
       this.map.addLayer(this.markers)
     }
   }
@@ -65,5 +106,25 @@ export default {
 <style>
 #map {
   height: 600px;
+}
+.marker-cluster-small {
+  background-color: rgba(140, 146, 226, 0.6);
+}
+.marker-cluster-small div {
+  background-color: rgba(57, 116, 204, 0.6);
+}
+.mycluster {
+  width: 40px;
+  height: 40px;
+  background-color: greenyellow;
+  text-align: center;
+  font-size: 24px;
+}
+.myclusterAlarm {
+  width: 40px;
+  height: 40px;
+  background-color: rgb(255, 130, 47);
+  text-align: center;
+  font-size: 24px;
 }
 </style>
